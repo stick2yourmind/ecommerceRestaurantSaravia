86ef6cb4500220b3d9672d74cb6df53af87629f3
Max Saravia <saravia.max.stk2@gmail.com>
2022-01-15 15:20:03 -0300

    added restaurant name as subtitle

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> Curso de React - Comisión 24895 - Coder House</h1>
+        <h2>  Restaurant Saravia  </h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
